e61e5a4c0de5ac193f1327aaec17537572c33d51
Tran Xuan Dong <113510151+dong-0404@users.noreply.github.com>
2024-12-21 15:28:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Ai engineer');
+  res.send('Hello World! Ai engineer, Tuýt ăn cức');
 });
 
 app.listen(port, () => {
