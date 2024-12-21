f378529cc99e6b600427a358a4e2f732d516a723
Tran Dong <dong.tran@monkey.edu.vn>
2024-12-21 21:07:26 +0700

    update index

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use('/api', userRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
